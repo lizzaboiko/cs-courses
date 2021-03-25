c47624e070769701b831358cda596a0cc82581f3
lizza <admin@MacBook-Air-lizza.local>
2021-03-25 20:53:02 +0300

    dezeshka1

---
+++ cs-courses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dezeshka1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Enter your lastname:");
+            var lastname = Console.ReadLine();
+
+            Console.WriteLine("Enter your birth year:");
+            var birthyear = int.Parse(Console.ReadLine());
+
+            var age = DateTime.Now.Year - birthyear;
+
+            Console.WriteLine("Hello, " + name + " " + lastname + "! Your age is " + age);
+        }
+    }
+}
